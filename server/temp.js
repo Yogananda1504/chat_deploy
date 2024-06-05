11a24d9e1a6dc64f004a6639f3bcc004e4e2fc51
Eurrus <vpavithra@students.vnit.ac.in>
2024-06-05 20:41:42 +0530

    #

--- server/temp.js
+++ server/temp.js
@@ -1,4 +1,3 @@
-require("dotenv").config();
 const express = require("express");
 const http = require("http");
 const cors = require("cors");
@@ -6,6 +5,7 @@ const socketIO = require("socket.io");
 const mongoose = require("mongoose");
 const redisAdapter = require("socket.io-redis");
 const path = require("path");
+require('dotenv').config();
 
 // Require routers
 const chatRouter = require("./routes/chatRouter");
@@ -16,44 +16,43 @@ const { handleSocketEvents } = require("./controllers/socketController");
 const app = express();
 const server = http.createServer(app);
 const io = socketIO(server, {
-	cors: {
-		origin: process.env.AZURE_DOMAIN || "http://localhost:5173", // Use environment variable or fallback to localhost for development
-		methods: ["GET", "POST"],
-	},
+    cors: {
+        origin: process.env.AZURE_DOMAIN || "http://localhost:5173",
+        methods: ["GET", "POST"],
+    },
 });
 
 app.use(cors());
 app.use(express.json());
 
 // MongoDB connection
-const MONGO_DB_URI =
-	process.env.MONGO_DB_URI || "mongodb://127.0.0.1:27017/chat";
+const MONGO_DB_URI = process.env.MONGO_DB_URI || "mongodb://127.0.0.1:27017/chat";
 mongoose.connect(MONGO_DB_URI, {
-	useNewUrlParser: true,
-	useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
 });
 const db = mongoose.connection;
 db.on("error", (error) => {
-	console.error("MongoDB connection error:", error);
+    console.error("MongoDB connection error:", error);
 });
 db.once("open", () => {
-	console.log("Successfully connected to the Database");
+    console.log("Successfully connected to the Database");
 });
 
 // Mount routers
 app.use("/api", chatRouter);
 
 // Serve static files from the React app's build directory
-const buildPath = path.join(__dirname, "..", "client", "dist"); // Use __dirname to ensure relative path
+const buildPath = path.join(__dirname, "..", "client", "dist");
 app.use(express.static(buildPath));
 
 // Redirect any non-API routes to the front end's index.html
 app.get("*", (req, res) => {
-	res.sendFile(path.join(buildPath, "index.html"));
+    res.sendFile(path.join(buildPath, "index.html"));
 });
 
 // Configure Redis adapter for Socket.IO
-const redisHost = process.env.REDIS_HOST || "localhost";
+const redisHost = process.env.REDIS_HOST || 'localhost';
 const redisPort = process.env.REDIS_PORT || 6379;
 io.adapter(redisAdapter({ host: redisHost, port: redisPort }));
 
@@ -63,5 +62,5 @@ handleSocketEvents(io);
 // Start the server on a worker process
 const port = process.env.PORT || 4000;
 server.listen(port, () => {
-	console.log(`Server running on port ${port}`);
+    console.log(`Server running on port ${port}`);
 });
